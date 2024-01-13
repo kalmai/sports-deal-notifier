110893012ad7fbe37d97ec542a8da949deb09c44
Kalmai <leozverres@gmail.com>
2024-01-12 23:56:29 -0500

    struggling with getting db out of recent insert

--- src/controller.cr
+++ src/controller.cr
@@ -21,13 +21,14 @@ module Controller
   # ## should probably break out the registration stuff to its own class or something...
   # should probably collapse these methods into one and add a checkbox on the FE to allow them to choose one/both?
   def register(context)
-    p! request_hash = request_hash(context)
-    # zipcode_id = find_zipcode_id(request_hash["zipcode"].as_s) # we will use this for the user eventually
+    request_hash = request_hash(context)
     contacts = JSON.parse request_hash.to_s
+    zipcode_id = find_zipcode_id(contacts["zipcode"].as_s) # we will use this for the user eventually
     new_contact_methods = Array(NewContactMethod).from_json contacts["contact_methods"].to_json
     new_contact_methods.each do |method|
       Database::Connection.exec("insert into contact_method (contact_type, contact_detail, enabled) values ($1, $2, $3)", method.contact_type, method.contact_details, method.enabled)
     end
+    debugger
     # record(NewContactMethod, t : String, detail : String, enabled : Bool)
     #  aas = Array(NewContactMethod).from_json(request_hash.to_s)
     # JobRunner.notify_of_deals
@@ -36,7 +37,16 @@ module Controller
   def find_zipcode_id(zip : String)
     # need to save data to our zipcode table in the future and check with db.query later
     # dont execute below if db.result otherwise crawl ballysport for the teams
+    zip_id = nil
+    rs = Database::Connection.query("select id, zipcode from address where zipcode = ($1)", zip)
+    rs.each do
+      p! zip_id = rs.read(Int)
+    end
 
+    return zip_id if zip_id
+
+    zip_id = Database::Connection.exec("insert into address (zipcode) values ($1)", zip)
+    debugger
     # https://forum.crystal-lang.org/t/how-to-dig-for-a-dynamic-json-value/3745/3
     json = Crawler.crawl_teams_from_zip(zip)
     # data = JSON::Parser.new(json).parse
